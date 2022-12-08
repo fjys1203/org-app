c2c17fa9229ff4008a080ceb35033f462c13c56d
fjys1203 <fjys.1203@gmail.com>
2022-12-08 11:19:10 +0900

    ユーザー機能の作成

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :blogs
+  has_many :meetings
+
+  validates :password,  presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :last_name, presence: true,  format: {with:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, presence: true,   format: {with:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+end
