a03f0618a90015c20a0aa0df4a262175b748549e
fjys1203 <fjys.1203@gmail.com>
2022-12-11 21:13:50 +0900

    デプロイエラー対応

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,5 +4,4 @@ class Blog < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :start_time, presence: true
-  validates :user, foreign_key: true
 end
